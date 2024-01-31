efe94b0ecc738b810d63ad2c70a7c82d58ff4bab
KlebMar <klebmarrok@gmail.com>
2024-01-30 20:26:31 -0500

    adding Remaing, Expense components

--- src/App.js
+++ src/App.js
@@ -16,15 +16,25 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                        // Budget component
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                         }        
 
                         {
-                            /* Add Remaining component here*/
+                            
+                    //Remaining component
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
                         }        
 
                         {
-                            /* Add ExpenseTotal component here */
+                    //ExpenseTotal component
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
                         }        
                        
                         {
